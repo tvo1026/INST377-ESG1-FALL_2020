ca099dab452fef7b7588618a9c2b325e1ec1bb02
coderbarn <coderbarn@gmail.com>
2020-10-28 10:21:15 -0400

    Some supporting code for lecture 8

--- public/Lecture_8_Support_Json_Ajax/js/fetchAPIexample.js
+++ public/Lecture_8_Support_Json_Ajax/js/fetchAPIexample.js
@@ -1,14 +1,41 @@
 // Promise
-const fetchPromise = fetch("https://api.umd.io/v0/bus/routes/115/locations");
+// const fetchPromise = fetch("https://api.umd.io/v0/bus/routes/115/locations");
 // Target main element
 const main = document.getElementById("main");
 
-fetchPromise.then(response => {
-    return response.json();
-  }).then(route => {
-      console.log(route);
-      const busId = route.vehicle.id;
-      console.log(busId);
-      // Append names to main element
-      main.innerHTML = "BusID: " + busId;
-  });
+function getBusRoute(){
+    
+    let busRoute = document.getElementById("busroute").value;
+    if(typeof busRoute !== "undefined" & busRoute !== ""){
+        console.log(busRoute);
+        let busURL = "https://api.umd.io/v0/bus/routes/" + busRoute;
+        console.log(busURL);
+        const fetchPromise = fetch(busURL);
+
+        fetchPromise.then(response => {
+            return response.json();
+        }).then(route => {
+            console.log("Here");
+            console.log(route);
+            let title, latMax, latMin, lonMax, lonMin;
+            let message = "";
+            if(typeof route.title !== "undefined"){
+                title = route.title;
+                latMax = route.lat_max;
+                latMin = route.lat_min;
+                lonMax = route.lon_max;
+                lonMin = route.lon_min;
+                message = title + " <br>Lat max : " + latMax + " <br>Lat min : " + latMin + " <br>Lon Max : " + lonMax + " <br>Lon Min :" + lonMin
+            }
+            else{
+                message = "No bus info available";
+            }
+            console.log(title);
+            // Append names to main element
+            main.innerHTML = "Bus Route Info: " + message;
+        });
+    }
+    else{
+        main.innerHTML = "No value provided";
+    }
+}
