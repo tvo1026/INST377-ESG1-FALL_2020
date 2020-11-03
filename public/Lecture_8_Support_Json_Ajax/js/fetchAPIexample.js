ab1d8a18cb496a8175453daf5fc69f027c11659c
pagarcia377 <60405634+pagarcia377@users.noreply.github.com>
2020-11-03 13:51:01 -0500

    Rewrite Lab 7 to use bus endpoint (#4)
    * Add error handling
    
    * Rewrite lab to use bus endpoint
    
    * Update HTML

--- public/Lecture_8_Support_Json_Ajax/js/fetchAPIexample.js
+++ public/Lecture_8_Support_Json_Ajax/js/fetchAPIexample.js
@@ -3,39 +3,51 @@
 // Target main element
 const main = document.getElementById("main");
 
-function getBusRoute(){
-    
-    let busRoute = document.getElementById("busroute").value;
-    if(typeof busRoute !== "undefined" & busRoute !== ""){
-        console.log(busRoute);
-        let busURL = "https://api.umd.io/v0/bus/routes/" + busRoute;
-        console.log(busURL);
-        const fetchPromise = fetch(busURL);
+function getBusRoute() {
+  let busRoute = document.getElementById("busroute").value;
+  if ((typeof busRoute !== "undefined") & (busRoute !== "")) {
+    console.log(busRoute);
+    let busURL = "https://api.umd.io/v0/bus/routes/" + busRoute;
+    console.log(busURL);
+    const fetchPromise = fetch(busURL);
 
-        fetchPromise.then(response => {
-            return response.json();
-        }).then(route => {
-            console.log("Here");
-            console.log(route);
-            let title, latMax, latMin, lonMax, lonMin;
-            let message = "";
-            if(typeof route.title !== "undefined"){
-                title = route.title;
-                latMax = route.lat_max;
-                latMin = route.lat_min;
-                lonMax = route.lon_max;
-                lonMin = route.lon_min;
-                message = title + " <br>Lat max : " + latMax + " <br>Lat min : " + latMin + " <br>Lon Max : " + lonMax + " <br>Lon Min :" + lonMin
-            }
-            else{
-                message = "No bus info available";
-            }
-            console.log(title);
-            // Append names to main element
-            main.innerHTML = "Bus Route Info: " + message;
-        });
-    }
-    else{
-        main.innerHTML = "No value provided";
-    }
+    fetchPromise
+      .then((response) => {
+        return response.json();
+      })
+      .then((route) => {
+        console.log("Here");
+        console.log(route);
+        let title, latMax, latMin, lonMax, lonMin;
+        let message = "";
+        if (typeof route.title !== "undefined") {
+          title = route.title;
+          latMax = route.lat_max;
+          latMin = route.lat_min;
+          lonMax = route.lon_max;
+          lonMin = route.lon_min;
+          message =
+            title +
+            " <br>Lat max : " +
+            latMax +
+            " <br>Lat min : " +
+            latMin +
+            " <br>Lon Max : " +
+            lonMax +
+            " <br>Lon Min :" +
+            lonMin;
+        } else {
+          message = "No bus info available";
+        }
+        console.log(title);
+        // Append names to main element
+        main.innerHTML = "Bus Route Info: " + message;
+      })
+      .catch((err) => {
+        console.log(err);
+        main.innerHTML = "Invalid bus route";
+      });
+  } else {
+    main.innerHTML = "No value provided";
+  }
 }
